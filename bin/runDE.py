ade48b2dc3a6861448a18b31f25a8320692246dc
Alexis <alxs412@gmail.com>
2020-01-14 16:04:25 -0800

    changed runDE to use abundance.tsv files instead of h5

--- bin/runDE.py
+++ bin/runDE.py
@@ -61,7 +61,8 @@ def main(group1=None, group2=None, outDir=None, inDir=None, formula=None):
     R.assign('sampleTable',sampleTable)
 
     #locate kallisto files
-    R('files <- file.path(inDir, samples, "abundance.h5")')
+    #would be faster to use kallito abundance.h5 files 
+    R('files <- file.path(inDir, samples, "abundance.tsv")')
     R('all(file.exists(files))')
 
     #tximport conversion to gene
